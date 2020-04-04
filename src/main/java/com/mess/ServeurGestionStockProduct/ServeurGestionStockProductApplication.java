2eb1c7588d0add8a7e9b7a2b8f44c37e76239728
Messaouda Benchouikh <Massaouda.Benchouikh@modisprod.onmicrosoft.com>
2020-04-04 19:22:31 +0200

    first commit

---
+++ src/main/java/com/mess/ServeurGestionStockProduct/ServeurGestionStockProductApplication.java
@@ -0,0 +1,13 @@
+package com.mess.ServeurGestionStockProduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionStockProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGestionStockProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mess/ServeurGestionStockProduct/ServeurGestionStockProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mess.ServeurGestionStockProduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeurGestionStockProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
